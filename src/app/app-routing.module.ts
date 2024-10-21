6874a9faf22b30f36d81be5823f6708d04908732
andreaDevelope <andrea_develope@outlook.it>
2024-10-21 17:02:42 +0200

    implement auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -12,6 +13,7 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () =>
       import('../app/pages/home/home.module').then((m) => m.HomeModule),
+    canActivate: [AuthGuard],
   },
 ];
 
--- src/app/main-components/nav/nav.component.ts
+++ src/app/main-components/nav/nav.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '.app-nav',
+  selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.scss',
+  styleUrl: './nav.component.scss'
 })
-export class NavComponent {}
+export class NavComponent {
+
+}
